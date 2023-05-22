e1c372c3c542966ac6b657a76544439f3995169d
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-05-23 00:58:47 +0900

    非同期処理

--- src/views/DaylyView.vue
+++ src/views/DaylyView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { useHead } from '@vueuse/head'
-import TimeEntryForm from '../components/TimeEntryForm.vue';
-import AttendantStatus from '@/components/AttendantStatus.vue';
-import TardinessStatus from '@/components/TardinessStatus.vue';
-import { inject } from 'vue';
+import TimeEntryForm from '../components/TimeEntryForm.vue'
+import AttendantStatus from '@/components/AttendantStatus.vue'
+import TardinessStatus from '@/components/TardinessStatus.vue'
+import { inject } from 'vue'
+import axios from 'axios'
 
 useHead({
   title: '日次勤怠'
@@ -11,55 +12,62 @@ useHead({
 
 // 型定義
 interface TardinessStatusData {
-  defaultTardinessStatus:string
+  defaultTardinessStatus: string
 }
 
-interface AttendantStatusData{
-  defaultAttendantStatus:string
+interface AttendantStatusData {
+  defaultAttendantStatus: string
 }
 
-interface TimeEntryData{
-  startHour:number,
-  startMinute:number,
-  endHour:number,
-  endMinute:number,
-  restHour:number,
-  restMinute:number,
-  totalWorkHours:number,
+interface TimeEntryData {
+  startHour: number
+  startMinute: number
+  endHour: number
+  endMinute: number
+  restHour: number
+  restMinute: number
+  totalWorkHours: number
 }
 
 // 各コンポーネント間からデータ取得
 
-const tardinessStatusData =inject<TardinessStatusData>('tardinessStatusData',{
-  defaultTardinessStatus:''
+const tardinessStatusData = inject<TardinessStatusData>('tardinessStatusData', {
+  defaultTardinessStatus: ''
 })
-const attendantStatusData = inject<AttendantStatusData>('attendantStatusData',{
-  defaultAttendantStatus:''
-})
-
-const timeEntryData = inject<TimeEntryData>('timeEntryData',{
-  startHour:0,
-  startMinute:0,
-  endHour:0,
-  endMinute:0,
-  restHour:0,
-  restMinute:0,
-  totalWorkHours:0
+const attendantStatusData = inject<AttendantStatusData>('attendantStatusData', {
+  defaultAttendantStatus: ''
 })
 
-const formData = {tardinessStatusData,attendantStatusData,timeEntryData}
+const timeEntryData = inject<TimeEntryData>('timeEntryData', {
+  startHour: 0,
+  startMinute: 0,
+  endHour: 0,
+  endMinute: 0,
+  restHour: 0,
+  restMinute: 0,
+  totalWorkHours: 0
+})
 
+const formData = { tardinessStatusData, attendantStatusData, timeEntryData }
 
+const handleSubmit = async () => {
+  try {
+    const response = await axios.post('urlこれから整備', formData)
+    console.log(response.data)
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
   <h2>日次勤怠</h2>
-  <AttendantStatus/>
-  <br>
-  <TimeEntryForm/>
+  <AttendantStatus />
+  <br />
+  <TimeEntryForm />
   <TardinessStatus />
-  <br>
-  <div> 
-    <button @click="$router.push({ path: '/dayly/attendanceRegistration' })">登録する</button>
-  </div> 
+  <br />
+  <div>
+    <button @click="$router.push({ path: '/dayly/attendanceRegistration' }),handleSubmit">登録する</button>
+  </div>
 </template>
