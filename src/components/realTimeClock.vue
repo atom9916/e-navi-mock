dc920a1c2efdfb62847ca7d7b14e3630ed61dac3
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-05-29 19:12:44 +0900

    コントローラーの修正

--- src/components/realTimeClock.vue
+++ src/components/realTimeClock.vue
@@ -3,34 +3,33 @@ import { ref, onMounted } from 'vue'
 
 const currentDate = ref('')
 const currentTime = ref('')
-const now = new Date()
 
-const updateDateTime = (now: Date) => {
-  currentDate.value = now.toLocaleDateString()
-  currentTime.value = now.toLocaleTimeString()
+const updateDateTime = () => {
+  const now = new Date()
+  currentDate.value = now.toLocaleDateString() 
+  currentTime.value = now.toLocaleTimeString() 
 }
 
 onMounted(() => {
-  updateDateTime(now)
+  updateDateTime()
   setInterval(updateDateTime, 1000)
 })
 </script>
 
 <template>
-  <section aria-labelledby="today-contents">
-    <p id="today-contents">
-      今日は&nbsp;<span style="font-weight: 500; font-size: 1.4em; color: red">
-        {{ currentDate }} </span
+  <div>
+    <p>
+      今日は&nbsp;<span style="font-weight: 500; font-size: 1.4em; color: red">{{
+        currentDate
+      }}</span
       >&nbsp;です
     </p>
-  </section>
-  <section aria-labelledby="now-contents">
-    <p id="now-contents">
+  </div>
+  <div>
+    <p>
       現在の時刻は&nbsp;<span style="font-weight: 250; font-size: 1.4em">{{ currentTime }}</span
       >&nbsp;です
     </p>
-  </section>
-  <section aria-labelledby="encouragement-message">
-    <p id="encouragement-message">今日もお仕事、がんばりましょう…</p>
-  </section>
+  </div>
+  <div><p>今日もお仕事、がんばりましょう…</p></div>
 </template>
