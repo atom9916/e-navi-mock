1fca31b69e24ce52a367c914126cafeaacb7b3e0
yukatakahashi <yuka.takahashi@rakus-partners.co.jp>
2023-06-13 17:02:20 +0900

    ログインフォームのCSSを実装しました

--- src/components/ComponentButton.vue
+++ src/components/ComponentButton.vue
@@ -19,14 +19,17 @@ const props = defineProps({
   border: none;
   cursor: pointer;
   color: white;
+  margin: 10px 0;
+  padding: 7px;
   font-weight: 700;
-  width: 80%;
+  font-size: 16px;
+  width: 100%;
   border-radius: 5px;
   border-bottom: 4px solid rgb(5, 57, 20);
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
 }
 .button:hover {
-  margin-top: 5px;
+  margin-top: 13px;
   background-color: rgb(35, 172, 74);
   border-bottom: 1px solid rgb(4, 44, 15);
 }
